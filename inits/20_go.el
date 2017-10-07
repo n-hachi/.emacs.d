32065843e0db3cfc4e74babe73f1a14c15eb1013
Nozomu Hatta <nozomuhatta@gmail.com>
2017-10-07 16:52:38 +0900

    go-autocompleteを追加した

--- inits/20_go.el
+++ inits/20_go.el
@@ -1,7 +1,7 @@
+(require 'go-autocomplete)
+
 (add-hook 'go-mode-hook (lambda()
             (setq gofmt-command "goimports")
-            (setq c-basic-offset 4)
-            (setq tab-width 2 indent-tabs-mode t)
             (local-set-key (kbd "M-.") 'godef-jump)
             (add-hook 'before-save-hook 'gofmt-before-save)
             ))
