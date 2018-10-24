490b460789c1fd533628d2769be204b54e8dc404
Nozomu Hatta <nozomuhatta@gmail.com>
2018-10-25 04:21:24 +0900

    Enable complementation.

--- inits/20_go.el
+++ inits/20_go.el
@@ -1,6 +1,10 @@
 (require 'go-autocomplete)
 
 (add-hook 'go-mode-hook (lambda()
+            ;; gocode
+            (auto-complete-mode 1)
+            (setq ac-sources '(ac-source-go))
+
             (setq gofmt-command "goimports")
             (local-set-key (kbd "M-.") 'godef-jump)
             (local-set-key (kbd "M-,") 'pop-tag-mark)
